d463b827189cd35b5f8832f8fd09078fe65386a2
Doğanay Tay <doganaytay@gmail.com>
2023-07-28 17:15:03 +0300

    Calculate BMI

---
+++ Calculate_BMI/main.py
@@ -0,0 +1,13 @@
+def bmi(weight, height):
+    bmi = weight / height ** 2
+    if bmi <= 18.5:
+        return "Underweight"
+    elif bmi <= 25:
+        return "Normal"
+    elif bmi <= 30:
+        return "Overweight"
+    else:
+        return "Obese"
+
+
+print(bmi(200, 1.80))
